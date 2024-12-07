0f07f8c5918f7d3e9ed3ff66cc06ceaf1fc855eb
r1nkez <dartapchihilus@gmail.com>
2024-12-07 21:44:28 +0500

    Финал

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Portfolio extends Model
 {
     protected $fillable = [
-        'name', 'price', 'val'
+        'name', 'price', 'val', 'category'
     ];
 }
